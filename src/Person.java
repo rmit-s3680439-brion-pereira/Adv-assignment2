8c6418b1e605f00ff85d0bdc1bf729428ddc85c6
s3667376-abhilash-nunes <32349531+s3667376-abhilash-nunes@users.noreply.github.com>
2018-05-18 16:10:13 +1000

    Person class

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,61 @@
 
+import java.sql.Blob;
+
 public class Person {
+
+	private String name;
+	private Integer age;
+	private String status;
+	private String gender;
+	private String state;
+	private Blob photo;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Blob getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(Blob photo) {
+		this.photo = photo;
+	}
 
 }
